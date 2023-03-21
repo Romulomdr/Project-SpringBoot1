51d40b750b74f3a31323af8d3312f0d5a171c8e9
Romulomdr <romulomatheus31@gmail.com>
2023-03-21 14:44:30 -0300

    Paymente Status, One-To-One Association

--- src/main/java/com/romuloM/curso/config/TestConfig.java
+++ src/main/java/com/romuloM/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.romuloM.curso.entities.Category;
 import com.romuloM.curso.entities.Order;
 import com.romuloM.curso.entities.OrderItem;
+import com.romuloM.curso.entities.Payment;
 import com.romuloM.curso.entities.Product;
 import com.romuloM.curso.entities.User;
 import com.romuloM.curso.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
